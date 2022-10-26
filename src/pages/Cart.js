2d091389ed3a98b04965a3eab3ac5d7771e6e009
Mohammed Abraar <mdabraargm@gmail.com>
2022-10-26 20:00:41 +0400

    fix: added singleProduct page add to cart functionality

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -3,9 +3,13 @@ import { useContext, useEffect, useState } from "react";
 import { CartContext } from "../CartContext";
 
 const Cart = () => {
+  let total = 0;
+
   const [products, setProducts] = useState([]);
 
   const { cart, setCart } = useContext(CartContext);
+
+  const [priceFetched, togglePriceFetched] = useState(false);
 
   const getQuantity = (productId) => {
     return cart.items[productId];
@@ -14,15 +18,58 @@ const Cart = () => {
   const increment = (productId) => {
     const existingQuantity = cart.items[productId];
     const _cart = { ...cart };
-    _cart.items[productId] += existingQuantity;
-    _cart.totalItems++;
+    _cart.items[productId] = existingQuantity + 1;
+    _cart.totalItems += 1;
     setCart(_cart);
+  };
+
+  const decrement = (productId) => {
+    const existingQuantity = cart.items[productId];
+    const _cart = { ...cart };
+    if (existingQuantity === 1) {
+      return;
+    }
+    _cart.items[productId] = existingQuantity - 1;
+    _cart.totalItems -= 1;
+    setCart(_cart);
+  };
+
+  const getSum = (quantity, price) => {
+    const sum = quantity * price;
+    total += sum;
+    return sum;
+  };
+
+  const handleDelete = (productId) => {
+    const _cart = { ...cart };
+    const quantity = _cart.items[productId];
+    delete _cart.items[productId];
+
+    _cart.totalItems -= quantity;
+
+    setCart(_cart);
+    setProducts(
+      products.filter((product) => {
+        return product._id !== productId;
+      })
+    );
+  };
+
+  const handleOrderNow = () => {
+    window.alert("Order placed successfully.");
+    setProducts([]);
+    setCart({});
   };
 
   useEffect(() => {
     if (!cart.items) {
       return;
     }
+
+    if (priceFetched) {
+      return;
+    }
+
     fetch(`https://ecom-rest-apis.herokuapp.com/api/products/cart-items`, {
       method: "POST",
       headers: {
@@ -35,23 +82,29 @@ const Cart = () => {
       })
       .then((products) => {
         setProducts(products);
+        togglePriceFetched(true);
       });
   }, [cart]);
 
-  return (
+  return products.length ? (
     <div className="container mx-auto lg:w-1/2 w-full pb-24">
       <h1 className="my-12 font-bold">Cart Items</h1>
       <ul>
         {products.map((product) => {
           return (
-            <li className="mb-12">
+            <li className="mb-12" key={product._id}>
               <div className="flex items-center justify-between">
                 <div className="flex items-center">
                   <img className="h-16" src={product.image} alt="" />
                   <span className="font-bold ml-4 w-48">{product.name}</span>
                 </div>
                 <div>
-                  <button className="bg-yellow-500 px-4 py-2 rounded-full leading-none">
+                  <button
+                    onClick={() => {
+                      decrement(product._id);
+                    }}
+                    className="bg-yellow-500 px-4 py-2 rounded-full leading-none"
+                  >
                     -
                   </button>
                   <b className="px-4">{getQuantity(product._id)}</b>
@@ -64,8 +117,15 @@ const Cart = () => {
                     +
                   </button>
                 </div>
-                <span> AED {product.price}</span>
-                <button className="bg-red-500 px-4 py-2 rounded-full leading-none text-white">
+                <span>
+                  AED {getSum(getQuantity(product._id), product.price)}
+                </span>
+                <button
+                  onClick={() => {
+                    handleDelete(product._id);
+                  }}
+                  className="bg-red-500 px-4 py-2 rounded-full leading-none text-white"
+                >
                   Delete
                 </button>
               </div>
@@ -75,14 +135,23 @@ const Cart = () => {
       </ul>
       <hr className="my-6" />
       <div className="text-right">
-        <b> Grand Total:</b> AED 1000
+        <b> Grand Total:</b> AED {total}
       </div>
       <div className="text-right mt-6">
-        <button className="bg-yellow-500 px-4 py-2 rounded-full leading-none">
+        <button
+          onClick={handleOrderNow}
+          className="bg-yellow-500 px-4 py-2 rounded-full leading-none"
+        >
           Order Now
         </button>
       </div>
     </div>
+  ) : (
+    <img
+      className="mx-auto w-1/2 mt-12"
+      src="/images/empty-cart.png"
+      alt="empty-cart"
+    />
   );
 };
 
--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,12 +1,42 @@
-import { useEffect, useState } from "react";
-import { useParams, Link, useHistory } from "react-router-dom";
+import { useEffect, useState, useContext } from "react";
+import { useParams, useHistory } from "react-router-dom";
+
+import { CartContext } from "../CartContext";
 
 const SingleProduct = () => {
+  const { cart, setCart } = useContext(CartContext);
+
+  const [isAdding, setIsAdding] = useState(false);
+
   const [product, setProduct] = useState({});
 
   const params = useParams();
 
   const history = useHistory();
+
+  const addToCart = (event, product) => {
+    event.preventDefault();
+    let _cart = { ...cart };
+    if (!_cart.items) {
+      _cart.items = {};
+    }
+    if (_cart.items[product._id]) {
+      _cart.items[product._id] += 1;
+    } else {
+      _cart.items[product._id] = 1;
+    }
+
+    if (!_cart.totalItems) {
+      _cart.totalItems = 0;
+    }
+    _cart.totalItems += 1;
+
+    setCart(_cart);
+    setIsAdding(true);
+    setTimeout(() => {
+      setIsAdding(false);
+    }, 1000);
+  };
 
   useEffect(() => {
     fetch(`https://ecom-rest-apis.herokuapp.com/api/products/${params._id}`)
@@ -34,8 +64,16 @@ const SingleProduct = () => {
           <h1 className="text-xl font-bold">{product.name}</h1>
           <div className="text-md">{product.size}</div>
           <div className="font-bold mt-2">AED {product.price}</div>
-          <button className="bg-yellow-500 py-1 px-8 rounded-full font-bold mt-4">
-            Add to cart
+          <button
+            disabled={isAdding}
+            onClick={(e) => {
+              addToCart(e, product);
+            }}
+            className={`${
+              isAdding ? "bg-green-500" : "bg-yellow-500"
+            } py-1 px-4 rounded-full font-bold`}
+          >
+            {isAdding ? "Added to Cart" : "Add to Cart"}
           </button>
         </div>
       </div>
